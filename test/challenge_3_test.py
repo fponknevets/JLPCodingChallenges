ecab409bca73e0ffd2cbfbf00bf7bb2ff1b93f6c
fponknevets <steven.knopf@gmail.com>
2020-02-22 11:53:38 +0000

    after train ride changes

--- test/challenge_3_test.py
+++ test/challenge_3_test.py
@@ -1,32 +1,23 @@
 import unittest
-
 from challenge_3 import process_sokoban_move
-
 
 
 class TestChallenge3(unittest.TestCase):
 
-    def setUp(self):
+    def test_process_sokoban_move_exists(self):
+        self.assertEqual(process_sokoban_move(), None)
+
+    def test_returns_the_board(self):
         board = ["#############",
                 "#p        * #",
                 "#     b  b  #",
                 "# *         #",
                 "#############",
                 ]
-
-
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_process_sokoban_move_exists(self):
-        self.assertEqual(process_sokoban_move(), None)
-
-    def test_returns_the_board(self):
         self.assertEqual(process_sokoban_move(board), board)
 
     def test_accepts_board_and_move(self):
         pass
-
 
 
 if __name__ == "__main__":
