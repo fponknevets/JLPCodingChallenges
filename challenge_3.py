21c0d210775a3d93583f47f57cf5e5c293bb3880
fponknevets <steven.knopf@gmail.com>
2020-02-24 07:22:05 +0000

    weekend work

--- challenge_3.py
+++ challenge_3.py
@@ -1,3 +1,97 @@
+from app import challenge_3_constants as consts
 
-def process_sokoban_move(board = None):
-    return board
+def process_sokoban_move(board=None, move=None):
+
+    def _board_as_grid(board):
+        grid = []
+        for line, line_content in enumerate(board):
+            grid.append([])
+            for square, square_content in enumerate(line_content):
+                grid[line].append(square_content)
+        return grid
+
+    def _grid_as_board(grid):
+        board = []
+        for line in grid:
+            line_content = ''
+            for square in line:
+                line_content += square
+            board.append(line_content)
+        return board
+
+    def _find_player():
+        for line, line_content in enumerate(grid):
+            for square, square_content in enumerate(line_content):
+                if square_content == (consts.PLAYER or
+                               consts.PLAYER_ON_STORAGE_LOCATION):
+                    return line, square, square_content
+        raise RuntimeError('Player not found!')
+
+    def _whats_above(line, square):
+        return int(line-1), int(square), grid[line-1][square]
+
+    def _whats_below(line, square):
+        return int(line+1), int(square), grid[line+1][square]
+
+    def _whats_left(line, square):
+        return int(line), int(square-1), grid[line][square-1]
+
+    def _whats_right(line, square):
+        return int(line), int(square+1), grid[line][square+1]
+
+    def _whats_at(line, square, move):
+        if move == consts.UP:
+            return _whats_above(line, square)
+        elif move == consts.DOWN:
+            return _whats_below(line, square)
+        elif move == consts.LEFT:
+            return _whats_left(line, square)
+        elif move == consts.RIGHT:
+            return _whats_right(line, square)
+
+    def _write(line, square, character):
+        grid[line][square] = character
+
+    grid = _board_as_grid(board)
+
+    try:
+        player_line, player_square, player_square_content = _find_player()
+    except RuntimeError:
+        print('Player not found!')
+        return False
+
+    target_line, target_square, target_content = _whats_at(player_line, player_square, move)
+
+    if target_content == consts.WALL:
+        return board
+
+    if target_content == consts.BOX:
+        passed_box_line, passed_box_square, passed_box_content = _whats_at(target_line, target_square, move)
+        if passed_box_content == consts.WALL:
+            return board
+
+        if passed_box_content == consts.EMPTY:
+            _write(passed_box_line, passed_box_square, consts.BOX)
+            _write(target_line, target_square, consts.PLAYER)
+            _write(player_line, player_square, consts.EMPTY)
+        elif passed_box_content == consts.STORAGE_LOCATION:
+            _write(passed_box_line, passed_box_square, consts.BOX_ON_STORAGE_LOCATION)
+            _write(target_line, target_square, consts.PLAYER)
+            _write(player_line, player_square, consts.EMPTY)
+        return _grid_as_board(grid)
+
+    if target_content == consts.STORAGE_LOCATION:
+        _move_player()
+
+    if target_content == consts.EMPTY:
+        grid[target_line][target_square] = consts.PLAYER
+    elif target_content == consts.STORAGE_LOCATION:
+        grid[target_line][target_square] = consts.PLAYER_ON_STORAGE_LOCATION
+
+    if grid[player_line][player_square] == consts.PLAYER:
+        grid[player_line][player_square] = consts.EMPTY
+    elif grid[player_line][player_square] == consts.PLAYER_ON_STORAGE_LOCATION:
+        grid[player_line][player_square] = consts.PLAYER_ON_STORAGE_LOCATION
+
+    return _grid_as_board(grid)
+
--- test/challenge_3_test.py
+++ test/challenge_3_test.py
@@ -4,20 +4,103 @@ from challenge_3 import process_sokoban_move
 
 class TestChallenge3(unittest.TestCase):
 
-    def test_process_sokoban_move_exists(self):
-        self.assertEqual(process_sokoban_move(), None)
+    def test_processes_legal_up_move(self):
+        board_in = ["#############",
+                    "#         * #",
+                    "#p    b  b  #",
+                    "# *         #",
+                    "#############",]
+        move = 'U'
+        board_out = ["#############",
+                     "#p        * #",
+                     "#     b  b  #",
+                     "# *         #",
+                     "#############",]
+        self.assertEqual(process_sokoban_move(board_in, move), board_out)
 
-    def test_returns_the_board(self):
-        board = ["#############",
-                "#p        * #",
-                "#     b  b  #",
-                "# *         #",
-                "#############",
-                ]
-        self.assertEqual(process_sokoban_move(board), board)
+    def test_processes_legal_down_move(self):
+        board_in = ["#############",
+                    "#         * #",
+                    "#p    b  b  #",
+                    "# *         #",
+                    "#############",]
+        move = 'D'
+        board_out = ["#############",
+                     "#         * #",
+                     "#     b  b  #",
+                     "#p*         #",
+                     "#############",]
+        self.assertEqual(process_sokoban_move(board_in, move), board_out)
 
-    def test_accepts_board_and_move(self):
-        pass
+    def test_processes_legal_right_move(self):
+        board_in = ["#############",
+                    "#         * #",
+                    "#p    b  b  #",
+                    "# *         #",
+                    "#############",]
+        move = 'R'
+        board_out = ["#############",
+                     "#         * #",
+                     "# p   b  b  #",
+                     "# *         #",
+                     "#############",]
+        self.assertEqual(process_sokoban_move(board_in, move), board_out)
+
+    def test_processes_legal_left_move(self):
+        board_out = ["#############",
+                     "#         * #",
+                     "#p    b  b  #",
+                     "# *         #",
+                     "#############",]
+        move = 'L'
+        board_in = ["#############",
+                    "#         * #",
+                    "# p   b  b  #",
+                    "# *         #",
+                    "#############",]
+        self.assertEqual(process_sokoban_move(board_in, move), board_out)
+
+    def test_does_not_move_into_wall(self):
+        board_in = ["#############",
+                    "#         * #",
+                    "#p    b  b  #",
+                    "# *         #",
+                    "#############",]
+        move = 'L'
+        board_out = ["#############",
+                     "#         * #",
+                     "#p    b  b  #",
+                     "# *         #",
+                     "#############",]
+        self.assertEqual(process_sokoban_move(board_in, move), board_out)
+
+    def test_can_push_box_to_empty_square(self):
+        board_in = ["#############",
+                    "#         * #",
+                    "#    pb  b  #",
+                    "# *         #",
+                    "#############",]
+        move = 'R'
+        board_out = ["#############",
+                     "#         * #",
+                     "#     pb b  #",
+                     "# *         #",
+                     "#############",]
+        self.assertEqual(process_sokoban_move(board_in, move), board_out)
+
+    def test_can_push_box_to_storage_square(self):
+        board_in = ["#############",
+                    "#         * #",
+                    "#    pb* b  #",
+                    "# *         #",
+                    "#############",]
+        move = 'R'
+        board_out = ["#############",
+                     "#         * #",
+                     "#     pB b  #",
+                     "# *         #",
+                     "#############",]
+        self.assertEqual(process_sokoban_move(board_in, move), board_out)
 
 
 if __name__ == "__main__":
